539c7ea80c22b33a0a371cfed88e38dc800a5eb1
Lyude <cpaul@redhat.com>
2016-07-26 13:34:41 -0400

    drm/i915/skl: Update plane watermarks atomically during plane updates
    Thanks to Ville for suggesting this as a potential solution to pipe
    underruns on Skylake.
    
    On Skylake all of the registers for configuring planes, including the
    registers for configuring their watermarks, are double buffered. New
    values written to them won't take effect until said registers are
    "armed", which is done by writing to the PLANE_SURF (or in the case of
    cursor planes, the CURBASE register) register.
    
    With this in mind, up until now we've been updating watermarks on skl
    like this:
    
      non-modeset {
       - calculate (during atomic check phase)
       - finish_atomic_commit:
         - intel_pre_plane_update:
            - intel_update_watermarks()
         - {vblank happens; new watermarks + old plane values => underrun }
         - drm_atomic_helper_commit_planes_on_crtc:
            - start vblank evasion
            - write new plane registers
            - end vblank evasion
      }
    
      or
    
      modeset {
       - calculate (during atomic check phase)
       - finish_atomic_commit:
         - crtc_enable:
            - intel_update_watermarks()
         - {vblank happens; new watermarks + old plane values => underrun }
         - drm_atomic_helper_commit_planes_on_crtc:
            - start vblank evasion
            - write new plane registers
            - end vblank evasion
      }
    
    Now we update watermarks atomically like this:
    
      non-modeset {
       - calculate (during atomic check phase)
       - finish_atomic_commit:
         - intel_pre_plane_update:
            - intel_update_watermarks() (wm values aren't written yet)
         - drm_atomic_helper_commit_planes_on_crtc:
            - start vblank evasion
            - write new plane registers
            - write new wm values
            - end vblank evasion
      }
    
      modeset {
       - calculate (during atomic check phase)
       - finish_atomic_commit:
         - crtc_enable:
            - intel_update_watermarks() (actual wm values aren't written
              yet)
         - drm_atomic_helper_commit_planes_on_crtc:
            - start vblank evasion
            - write new plane registers
    	- write new wm values
            - end vblank evasion
      }
    
    So this patch moves all of the watermark writes into the right place;
    inside of the vblank evasion where we update all of the registers for
    each plane. While this patch doesn't fix everything, it does allow us to
    update the watermark values in the way the hardware expects us to.
    
    Changes since original patch series:
     - Remove mutex_lock/mutex_unlock since they don't do anything and we're
       not touching global state
     - Move skl_write_cursor_wm/skl_write_plane_wm functions into
       intel_pm.c, make externally visible
     - Add skl_write_plane_wm calls to skl_update_plane
     - Fix conditional for for loop in skl_write_plane_wm (level < max_level
       should be level <= max_level)
     - Make diagram in commit more accurate to what's actually happening
     - Add Fixes:
    
    Changes since v1:
     - Use IS_GEN9() instead of IS_SKYLAKE() since these fixes apply to more
       then just Skylake
     - Update description to make it clear this patch doesn't fix everything
     - Check if pipes were actually changed before writing watermarks
    
    Changes since v2:
     - Write PIPE_WM_LINETIME during vblank evasion
    
    Changes since v3:
     - Rebase against new SAGV patch changes
    
    Fixes: 2d41c0b59afc ("drm/i915/skl: SKL Watermark Computation")
    Signed-off-by: Lyude <cpaul@redhat.com>
    Cc: stable@vger.kernel.org
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Cc: Daniel Vetter <daniel.vetter@intel.com>
    Cc: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Cc: Hans de Goede <hdegoede@redhat.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/intel_drv.h
+++ drivers/gpu/drm/i915/intel_drv.h
@@ -1711,6 +1711,8 @@ void skl_ddb_get_hw_state(struct drm_i915_private *dev_priv,
 			  struct skl_ddb_allocation *ddb /* out */);
 int skl_enable_sagv(struct drm_i915_private *dev_priv);
 int skl_disable_sagv(struct drm_i915_private *dev_priv);
+void skl_write_cursor_wm(struct intel_crtc *intel_crtc);
+void skl_write_plane_wm(struct intel_crtc *intel_crtc, int plane);
 uint32_t ilk_pipe_pixel_rate(const struct intel_crtc_state *pipe_config);
 bool ilk_disable_lp_wm(struct drm_device *dev);
 int sanitize_rc6_option(struct drm_i915_private *dev_priv, int enable_rc6);
