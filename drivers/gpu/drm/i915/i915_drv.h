7afe9e8dc52e4f33dd28d826f250427b21c2945c
Chris Wilson <chris@chris-wilson.co.uk>
2017-02-16 15:55:34 -0800

    drm/i915: Reserve space in the global seqno during request allocation
    A restriction on our global seqno is that they cannot wrap, and that we
    cannot use the value 0. This allows us to detect when a request has not
    yet been submitted, its global seqno is still 0, and ensures that
    hardware semaphores are monotonic as required by older hardware. To
    meet these restrictions when we defer the assignment of the global
    seqno, we must check that we have an available slot in the global seqno
    space during request construction. If that test fails, we wait for all
    requests to be completed and reset the hardware back to 0.
    
    Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>
    Reviewed-by: Joonas Lahtinen <joonas.lahtinen@linux.intel.com>
    Link: http://patchwork.freedesktop.org/patch/msgid/20161028125858.23563-33-chris@chris-wilson.co.uk
    (cherry picked from commit 28176ef4cfa510e5f1498bbf39ff1e4afd0b085d)
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -2105,6 +2105,7 @@ struct drm_i915_private {
 
 		struct list_head timelines;
 		struct i915_gem_timeline global_timeline;
+		u32 active_requests;
 
 		/**
 		 * Is the GPU currently considered idle, or busy executing
@@ -2113,7 +2114,6 @@ struct drm_i915_private {
 		 * In order to reduce the effect on performance, there
 		 * is a slight delay before we do so.
 		 */
-		unsigned int active_engines;
 		bool awake;
 
 		/**
