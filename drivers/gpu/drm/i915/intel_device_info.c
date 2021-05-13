7cd9ae6e1e08e91acccf93429546292446b0c5e5
Lucas De Marchi <lucas.demarchi@intel.com>
2021-05-13 15:59:03 -0700

    drm/i915: Add "release id" version
    Besides the arch version returned by GRAPHICS_VER(), new platforms
    contain a "release id" to make clear the difference from one platform to
    another. Although for the first ones we may use them as if they were a
    major/minor version, that is not true for all platforms: we may have a
    `release_id == n` that is closer to `n - 2` than to `n - 1`.
    
    However the release id number is not defined by hardware until we start
    using the GMD_ID register. For the platforms before that register is
    useful we will set the values in software and we can set them as we
    please. So the plan is to set them so we can group different features
    under a single GRAPHICS_VER_FULL() check.
    
    After GMD_ID is used, the usefulness of a "full version check" will be
    greatly reduced and will be mostly used for deciding workarounds and a
    few code paths. So it makes sense to keep it as a separate field from
    graphics_ver.
    
    Also, currently there is not much use for the release id in media and
    display, so keep them out.
    
    This is a mix of 2 independent changes: one by me and the other by Matt
    Roper.
    
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/intel_device_info.c
+++ drivers/gpu/drm/i915/intel_device_info.c
@@ -97,7 +97,9 @@ void intel_device_info_print_static(const struct intel_device_info *info,
 				    struct drm_printer *p)
 {
 	drm_printf(p, "graphics_ver: %u\n", info->graphics_ver);
+	drm_printf(p, "graphics_ver_release: %u\n", info->graphics_ver_release);
 	drm_printf(p, "media_ver: %u\n", info->media_ver);
+	drm_printf(p, "media_ver_release: %u\n", info->media_ver_release);
 	drm_printf(p, "display_ver: %u\n", info->display.ver);
 	drm_printf(p, "gt: %d\n", info->gt);
 	drm_printf(p, "iommu: %s\n", iommu_name());
