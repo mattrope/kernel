c621a055dfb550f10bcce236e5ebdd0bc74bec59
Matt Roper <matthew.d.roper@intel.com>
2025-11-06 16:07:58 -0800

    drm/xe/vram: Use scope-based forcewake
    Switch VRAM code to use scope-based forcewake for consistency with other
    parts of the driver.
    
    Signed-off-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/xe/xe_vram.c
+++ drivers/gpu/drm/xe/xe_vram.c
@@ -245,7 +245,6 @@ static int tile_vram_size(struct xe_tile *tile, u64 *vram_size,
 {
 	struct xe_device *xe = tile_to_xe(tile);
 	struct xe_gt *gt = tile->primary_gt;
-	unsigned int fw_ref;
 	u64 offset;
 	u32 reg;
 
@@ -265,8 +264,8 @@ static int tile_vram_size(struct xe_tile *tile, u64 *vram_size,
 		return 0;
 	}
 
-	fw_ref = xe_force_wake_get(gt_to_fw(gt), XE_FW_GT);
-	if (!fw_ref)
+	CLASS(xe_force_wake, fw_ref)(gt_to_fw(gt), XE_FW_GT);
+	if (!fw_ref.domains)
 		return -ETIMEDOUT;
 
 	/* actual size */
@@ -288,8 +287,6 @@ static int tile_vram_size(struct xe_tile *tile, u64 *vram_size,
 
 	/* remove the tile offset so we have just the available size */
 	*vram_size = offset - *tile_offset;
-
-	xe_force_wake_put(gt_to_fw(gt), fw_ref);
 
 	return 0;
 }
